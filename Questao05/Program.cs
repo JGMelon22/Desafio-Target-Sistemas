28fc0a219b1704599ab383ed1a60cf428394497f
JGMelon22 <jgvasconcellos22@gmail.com>
2025-01-11 15:04:24 -0300

    chore: fix question 5

--- Questao05/Program.cs
+++ Questao05/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string originalString = "Linux User 123";
+
+string reversedString = ReverseString(originalString);
+
+Console.WriteLine("String original: " + originalString);
+Console.WriteLine("String invertida: " + reversedString);
+
+
+string ReverseString(string input)
+{
+    char[] charArray = input.ToCharArray();
+
+    int leftIndex = 0;
+    int rightIndex = charArray.Length - 1;
+
+    while (leftIndex < rightIndex)
+    {
+        char temp = charArray[leftIndex];
+        charArray[leftIndex] = charArray[rightIndex];
+        charArray[rightIndex] = temp;
+
+        leftIndex++;
+        rightIndex--;
+    }
+
+    return new string(charArray);
+}
